86f6139c9e8a40b4593e19a57a421ab0f7d9478f
patrick lawler <plawler92@gmail.com>
2019-09-07 05:19:09 -0500

    adding everything

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,41 @@
 function changePlayer(playerName){
 	var x = document.getElementById("portrait");
+	var video = document.getElementById('play-vid');
 	switch(playerName){
 		case "john":
 			x.src = "images/john-action-main.png"
-			changeStats(1,1,1,1);
+			video.src = "videos/JohnBest.mov";
+			changeStats(42,1,1,16);
 			break;
 		case "mex":
-			x.src = "images/mex-head2.jpg"
-			changeStats(2,2,2,2);
+			x.src = "images/mex-action.png"
+			video.src = "videos/MexBest.mov";
+			changeStats(26,2,2,7);
 			break;
 		case "steph":
-			x.src = "";
-			changeStats(1,1,1,1);
+			x.src = "images/steph-action.png";
+			video.src = "videos/StephBest.mov";
+			changeStats(41,1,1,29);
 			break;
 		case "maria":
-			x.src = "";
-			changeStats(1,1,1,1);
+			x.src = "images/maria-action.png";
+			video.src = "videos/MariaBest.mov";
+			changeStats(17,1,1,11);
 			break;
 		case "lily":
-			x.src = "";
-			changeStats(1,1,1,1);
+			x.src = "images/lily-action.png";
+			video.src = "videos/LilyBest.mov";
+			changeStats(13,1,1,4);
 			break;
 		case "josh":
-			x.src = "";
-			changeStats(1,1,1,1);
+			x.src = "images/josh-action.png";
+			video.src = "videos/JoshBest.mov";
+			changeStats(33,1,1,17);
 			break;
 		case "eli":
-			x.src = "";
-			changeStats(1,1,1,1);
+			x.src = "images/eli-action.png";
+			video.src = "videos/EliBest.mov";
+			changeStats(25,1,1,8);
 			break;
 	}
 	console.log('hello world');
